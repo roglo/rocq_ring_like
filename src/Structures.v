fad55c82ad5544e833af4e6b1052b377aadbc980
Daniel de Rauglaudre <daniel.de_rauglaudre@inria.fr>
2025-11-17 07:18:30 +0100

    -

--- src/Structures.v
+++ src/Structures.v
@@ -168,8 +168,7 @@ Notation "a ≤ b ≤ c" := (a ≤ b ∧ b ≤ c)%L (at level 70, b at next leve
 Inductive not_applicable := NA.
 
 Class ring_like_ord T {ro : ring_like_op T} :=
-  { rngl_ord_le_dec : ∀ a b : T, ({a ≤ b} + {¬ a ≤ b})%L;
-    rngl_ord_le_refl : ∀ a, (a ≤ a)%L;
+  { rngl_ord_le_refl : ∀ a, (a ≤ a)%L;
     rngl_ord_le_antisymm : ∀ a b, (a ≤ b → b ≤ a → a = b)%L;
     rngl_ord_le_trans : ∀ a b c, (a ≤ b → b ≤ c → a ≤ c)%L;
     rngl_ord_add_le_mono_l :
@@ -179,6 +178,7 @@ Class ring_like_ord T {ro : ring_like_op T} :=
         ∀ a b c d, (0 ≤ a ≤ c)%L → (0 ≤ b ≤ d)%L → (a * b ≤ c * d)%L;
     rngl_ord_mul_le_compat_nonpos :
         ∀ a b c d, (c ≤ a ≤ 0)%L → (d ≤ b ≤ 0)%L → (a * b ≤ c * d)%L;
+    rngl_ord_le_dec : ∀ a b : T, ({a ≤ b} + {¬ a ≤ b})%L;
     rngl_ord_not_le : ∀ a b, (¬ a ≤ b → a ≠ b ∧ b ≤ a)%L }.
 
 Class ring_like_prop T {ro : ring_like_op T} :=
